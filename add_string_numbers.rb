f75d31da0b21d6708fd084a94b327face2c716d8
Abhishek <abhishekbose87@gmail.com>
2024-12-20 14:47:49 +0530

    handle any amount of numbers separated by commas

--- add_string_numbers.rb
+++ add_string_numbers.rb
@@ -1,4 +1,7 @@
 def add_string_numbers(numbers)
   return 0 if numbers.strip.empty?
-  return numbers.strip.to_i
+  numbers_array = numbers.split(",").reject { |num| num.strip.empty? }
+  sum = 0
+  numbers_array.map { |num| sum += num.to_i }
+  return sum
 end
--- add_string_numbers_test.rb
+++ add_string_numbers_test.rb
@@ -20,4 +20,15 @@ describe "add string numbers" do
   it "works for a single number with spaces and newlines" do
     assert_equal 1, add_string_numbers(" 1   \n")
   end
+
+  it "works for a multiple numbers separated by commas" do
+    assert_equal 6, add_string_numbers("1,2,3")
+  end
+  it "works for a multiple numbers separated by commas with spaces and newlines" do
+    assert_equal 6, add_string_numbers(" 1   \n, 2 , 3")
+  end
+
+  it "works for a multiple numbers separated by commas with spaces and newlines with extra comma at the end" do
+    assert_equal 6, add_string_numbers(" 1   \n, 2 , 3, ")
+  end
 end
